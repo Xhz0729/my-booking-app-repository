00cb606b32d0748fc8c4ad54eaf7a645cfd772d9
Xiahui Zhang <xhz0729@gmail.com>
2024-10-17 16:54:35 -0500

    handle my form input changes and handle my form submit

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  // handle user input
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // handle submit
+  async function registerUser(ev) {
+    ev.preventDefault();
+    try {
+      await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      alert("Registration successful. Now you can log in");
+    } catch (e) {
+      alert("Registration failed. Please try again later");
+    }
+  }
+
   return (
     <div className="mt-6 grow flex items-center justify-around">
       <div className="mb-72">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-xl mx-auto">
-          <input type="text" placeholder="Doudou" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="Doudou"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
           <button className="login">Register</button>
           <div className="text-center py-2 text-zinc-500">
             Already have an account?{" "}
