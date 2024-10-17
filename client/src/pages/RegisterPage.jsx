5b6ab75589371454ccacce6a663efc164e0a83fb
Xiahui Zhang <xhz0729@gmail.com>
2024-10-16 22:47:36 -0500

    fix typo, change 'Login' to 'Register

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -5,12 +5,12 @@ const RegisterPage = () => {
   return (
     <div className="mt-6 grow flex items-center justify-around">
       <div className="mb-72">
-        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <h1 className="text-4xl text-center mb-4">Register</h1>
         <form className="max-w-xl mx-auto">
           <input type="text" placeholder="Doudou" />
           <input type="email" placeholder="your@email.com" />
           <input type="password" placeholder="password" />
-          <button className="login">Login</button>
+          <button className="login">Register</button>
           <div className="text-center py-2 text-zinc-500">
             Already have an account?{" "}
             <Link className="underline text-black" to={"/login"}>
