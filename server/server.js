aa6a3f1bafb42fbecdaae39cae8e970142e06321
Xiahui Zhang <xhz0729@gmail.com>
2024-10-17 16:48:12 -0500

    add register route
    use mongoose to connect with database, use .env and dotenv to configure db url and use post method to create new user

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import User from "./models/User.js";
+import bcrypt from "bcrypt";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -7,8 +13,24 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.get("/", (req, res) => {
   res.json({ message: "Hola from the backend server" });
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = await User.create({
+      name,
+      email,
+      password: bcrypt.hash(password, 10),
+    });
+    res.json(newUser);
+  } catch (e) {
+    res.status(400).json(e);
+  }
 });
 
 app.listen(PORT, () => {
