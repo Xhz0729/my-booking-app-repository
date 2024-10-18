7c67765b83b84b47a291e8d2fdae8721c1840c4b
Xiahui Zhang <xhz0729@gmail.com>
2024-10-17 23:28:30 -0500

    add login logic
    add formData state to track user input and connect the component with backend route

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,18 +1,57 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  // handle user input
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // handle user login submit
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    const { email, password } = formData;
+    try {
+      await axios.post("/login", { email, password });
+      alert("Successfully Login");
+    } catch (e) {
+      alert("Login Failed");
+    }
+  }
+
   return (
     <div className="mt-6 grow flex items-center justify-around">
       <div className="mb-72">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-xl mx-auto">
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
-          <button className="login">Login</button>
+        <form className="max-w-xl mx-auto" onSubmit={handleLoginSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="your@email.com"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <button className="login" type="submit">
+            Login
+          </button>
           <div className="text-center py-2 text-zinc-500">
             Don't have an account yet?{" "}
-            <Link className="underline text-black" to={"/register"}>Register Now</Link>
+            <Link className="underline text-black" to={"/register"}>
+              Register Now
+            </Link>
           </div>
         </form>
       </div>
