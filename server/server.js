401ba52db9cca41acfcc9d1358b84838166d13bf
Xiahui Zhang <xhz0729@gmail.com>
2024-10-17 23:27:11 -0500

    add the login route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hola from the backend server" });
 });
 
+// route for register a new user
 app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -34,6 +35,24 @@ app.post("/register", async (req, res) => {
     res.json(newUser);
   } catch (e) {
     res.status(400).json(e);
+  }
+});
+
+// route for login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  // find the user with the email
+  const matchedUser = await User.findOne({ email });
+  if (matchedUser) {
+    // compare the input password with the database stored password
+    const validPassword = bcrypt.compareSync(password, matchedUser.password);
+    if (validPassword) {
+      res.json("Login successful");
+    } else {
+      res.status(422).json("Wrong password");
+    }
+  } else {
+    res.status(404).json("User not found");
   }
 });
 
