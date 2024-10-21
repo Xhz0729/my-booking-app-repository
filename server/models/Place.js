f75dbad90693987fc45f52d6940f5dc705f0a696
Xiahui Zhang <xhz0729@gmail.com>
2024-10-21 18:50:37 -0500

    create place schema

---
+++ server/models/Place.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const PlaceSchema = new Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  title: String,
+  address: String,
+  photos: [String],
+  description: String,
+  amenities: [String],
+  extraInfo: String,
+  checkIn: Number,
+  checkOut: Number,
+  maxGuests: Number,
+});
+
+const PlaceModel = mongoose.model("Place", PlaceSchema);
+
+export default PlaceModel;
