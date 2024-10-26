42d86fae2be5b05a9d3d56fd28816e1c99be5963
Xiahui Zhang <xhz0729@gmail.com>
2024-10-26 17:05:07 -0500

    Render HomePage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
+import HomePage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import axios from "axios";
@@ -19,6 +20,7 @@ function App() {
         <div>
           <Routes>
             <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/account" element={<ProfilePage />} />
