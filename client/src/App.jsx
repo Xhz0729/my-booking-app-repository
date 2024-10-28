50327332022fc2294e9595baf92205093a548bc5
Xiahui Zhang <xhz0729@gmail.com>
2024-10-28 13:04:43 -0500

    Add react route for PlaceDetailsPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import { UserContextProvider } from "./context/UserContext";
 import ProfilePage from "./pages/ProfilePage";
 import PlacesPage from "./pages/PlacesPage";
 import AddPlaceForm from "./components/AddPlaceForm";
+import PlaceDetailsPage from "./pages/PlaceDetailsPage";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
@@ -27,6 +28,7 @@ function App() {
               <Route path="/account/listings" element={<PlacesPage />} />
               <Route path="/account/listings/new" element={<AddPlaceForm />} />
               <Route path="/account/listings/:id" element={<AddPlaceForm />} />
+              <Route path="/place/:id" element={<PlaceDetailsPage />} />
             </Route>
           </Routes>
         </div>
