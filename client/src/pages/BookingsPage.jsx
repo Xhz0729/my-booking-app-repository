48d50b546f5aa0b720c0fba6f7ccfe91c5ecfdea
Xiahui Zhang <xhz0729@gmail.com>
2024-10-30 18:50:35 -0500

    Add link to booking details page and render the first photo of the booking place

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import AccountNav from "../components/AccountNav";
+import { Link } from "react-router-dom";
+import Image from "../components/Image";
 
 const BookingsPage = () => {
   // State for bookingsData
@@ -17,6 +19,27 @@ const BookingsPage = () => {
     <div>
       {/* Render the AccountNav component */}
       <AccountNav />
+
+      {/* Render the list of bookings */}
+      <div className="mt-6 mx-4">
+        {bookingsData.map((booking) => (
+          <Link
+            to={`/account/bookings/${booking._id}`}
+            key={booking._id}
+            className="flex gap-4 bg-red-50 mb-4 p-4 rounded-2xl cursor-pointer"
+          >
+            {/* Render the place first photo */}
+            <div className="w-48">
+              {/* Render the Image component */}
+              <Image
+                src={booking.place.photos[0]}
+                alt=""
+                className="object-cover"
+              />
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
