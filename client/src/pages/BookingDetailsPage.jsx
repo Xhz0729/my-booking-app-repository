1613333972abaa8f1eb726894577200b1d119617
Xiahui Zhang <xhz0729@gmail.com>
2024-10-31 12:13:40 -0500

    Fetch booking data of matched booking with the id

--- client/src/pages/BookingDetailsPage.jsx
+++ client/src/pages/BookingDetailsPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const BookingDetailsPage = () => {
   // Get the booking ID from the URL
@@ -7,6 +8,18 @@ const BookingDetailsPage = () => {
 
   //State of booking
   const [booking, setBooking] = useState(null);
+
+  // Use axios to get the booking details
+  useEffect(() => {
+    axios.get("/bookings").then((response) => {
+      // Find the booking that matches the ID
+      const matchedBooking = response.data.find(({ _id }) => _id === id);
+      if (!matchedBooking) {
+        return;
+      }
+      setBooking(matchedBooking);
+    });
+  }, [id]);
 
   return <div>BookingDetailsPage</div>;
 };
