06add2078bc37572a03248b3a0e09dab8a98ebb7
Xiahui Zhang <xhz0729@gmail.com>
2024-10-31 13:37:04 -0500

    Render title and MapLink component

--- client/src/pages/BookingDetailsPage.jsx
+++ client/src/pages/BookingDetailsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import MapLink from "../components/MapLink";
 
 const BookingDetailsPage = () => {
   // Get the booking ID from the URL
@@ -14,14 +15,24 @@ const BookingDetailsPage = () => {
     axios.get("/bookings").then((response) => {
       // Find the booking that matches the ID
       const matchedBooking = response.data.find(({ _id }) => _id === id);
-      if (!matchedBooking) {
-        return;
-      }
       setBooking(matchedBooking);
     });
   }, [id]);
 
-  return <div>BookingDetailsPage</div>;
+  // If the booking is not found, return an empty string
+  if (!booking) {
+    return "";
+  }
+
+  return (
+    <div className="my-8">
+      {/* Render the booking place title */}
+      <h1 className="text-xl font-bold mb-2">{booking.place.title}</h1>
+
+      {/* Render the booking address */}
+      <MapLink address={booking.place.address} />
+    </div>
+  );
 };
 
 export default BookingDetailsPage;
