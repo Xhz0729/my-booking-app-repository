5d7e9a97de693b5929ba01cbf9342d907399f53c
Xiahui Zhang <xhz0729@gmail.com>
2024-10-31 00:12:48 -0500

    Render MapLink component on BookingPage component

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -4,6 +4,7 @@ import AccountNav from "../components/AccountNav";
 import { Link } from "react-router-dom";
 import Image from "../components/Image";
 import { format } from "date-fns";
+import MapLink from "../components/MapLink";
 
 const BookingsPage = () => {
   // State for bookingsData
@@ -24,74 +25,77 @@ const BookingsPage = () => {
       {/* Render the list of bookings */}
       <div className="mt-6 mx-4">
         {bookingsData.map((booking) => (
-          <Link
-            to={`/account/bookings/${booking._id}`}
-            key={booking._id}
-            className="flex gap-4 bg-red-50 mb-4 p-4 rounded-2xl cursor-pointer"
-          >
-            {/* Render the place first photo */}
-            <div className="w-48">
-              {/* Render the Image component */}
-              <Image
-                src={booking.place.photos[0]}
-                alt=""
-                className="object-cover"
-              />
-            </div>
-            <div className="grow-0 shrink">
-              {/* Render the booking place title */}
-              <h2 className="text-xl font-bold">{booking.place.title}</h2>
-              {/* Render the booking check-in and check-out dates */}
-              {format(new Date(booking.checkIn), "MMM dd, yyyy")} -{" "}
-              {format(new Date(booking.checkOut), "MMM dd, yyyy")}
-              {/* Render the booking place price */}
-              <p className="mt-1">Total price: ${booking.price.toFixed(2)}</p>
-              {/* Render the booking place check in&out time */}
-              <div className="flex gap-20 mt-1">
-                <div className="flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                    />
-                  </svg>
+          <div key={booking._id} className="bg-red-50 mb-4 p-4 rounded-2xl">
+            <Link
+              to={`/account/bookings/${booking._id}`}
+              className="flex gap-4 cursor-pointer"
+            >
+              {/* Render the place first photo */}
+              <div className="w-48">
+                {/* Render the Image component */}
+                <Image
+                  src={booking.place.photos[0]}
+                  alt=""
+                  className="object-cover"
+                />
+              </div>
+              <div className="grow-0 shrink">
+                {/* Render the booking place title */}
+                <h2 className="text-xl font-bold mb-2">{booking.place.title}</h2>
+                {/* Render the booking check-in and check-out dates */}
+                {format(new Date(booking.checkIn), "MMM dd, yyyy")} -{" "}
+                {format(new Date(booking.checkOut), "MMM dd, yyyy")}
+                {/* Render the booking place price */}
+                <p className="mt-2">Total price: ${booking.price.toFixed(2)}</p>
+                {/* Render the booking place check in&out time */}
+                <div className="flex gap-20 mt-2">
+                  <div className="flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
 
-                  <p>
-                    <b>Check-in:</b>
-                    {booking.place.checkIn}:00 PM
-                  </p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                    />
-                  </svg>
-                  <p>
-                    <b>Checkout:</b>
-                    {booking.place.checkOut}:00 AM
-                  </p>
+                    <p>
+                      <b>Check-in:</b>
+                      {booking.place.checkIn}:00 PM
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
+                    <p>
+                      <b>Checkout:</b>
+                      {booking.place.checkOut}:00 AM
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
+            </Link>
+            {/* Render the MapLink component */}
+            <MapLink address={booking.place.address} />
+          </div>
         ))}
       </div>
     </div>
