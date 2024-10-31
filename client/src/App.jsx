2f264eaa592f93da94bd9ebf40888d126dee5cdd
Xiahui Zhang <xhz0729@gmail.com>
2024-10-31 17:42:20 -0500

    Fix the multiple call of map api
    Put the LoadScript at App.jsx to avoid calling map api times

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LoadScript } from "@react-google-maps/api";
 import "./App.css";
 import Layout from "./components/Layout";
 import HomePage from "./pages/Homepage";
@@ -19,29 +20,37 @@ axios.defaults.withCredentials = true;
 function App() {
   return (
     <UserContextProvider>
-      <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/account" element={<ProfilePage />} />
-              <Route path="/account/listings" element={<PlacesPage />} />
-              <Route path="/account/listings/new" element={<AddPlaceForm />} />
-              <Route path="/account/listings/:id" element={<AddPlaceForm />} />
-              <Route path="/place/:id" element={<PlaceDetailsPage />} />
-              {/* Add a route for the bookings page */}
-              <Route path="/account/bookings" element={<BookingsPage />} />
-              {/* Add a route for the booking details page */}
-              <Route
-                path="/account/bookings/:id"
-                element={<BookingDetailsPage />}
-              />
-            </Route>
-          </Routes>
-        </div>
-      </Router>
+      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <Router>
+          <div>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/account" element={<ProfilePage />} />
+                <Route path="/account/listings" element={<PlacesPage />} />
+                <Route
+                  path="/account/listings/new"
+                  element={<AddPlaceForm />}
+                />
+                <Route
+                  path="/account/listings/:id"
+                  element={<AddPlaceForm />}
+                />
+                <Route path="/place/:id" element={<PlaceDetailsPage />} />
+                {/* Add a route for the bookings page */}
+                <Route path="/account/bookings" element={<BookingsPage />} />
+                {/* Add a route for the booking details page */}
+                <Route
+                  path="/account/bookings/:id"
+                  element={<BookingDetailsPage />}
+                />
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+      </LoadScript>
     </UserContextProvider>
   );
 }
