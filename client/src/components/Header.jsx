ef71e1c151ecdfdf48c879263909ad65c14e09d9
Xiahui Zhang <xhz0729@gmail.com>
2024-11-04 22:48:21 -0600

    Update my app logo and color of the app's name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,22 +10,21 @@ const Header = () => {
       <header className="p-6 flex justify-between">
         {/* icon and company name */}
         {/* click the icon to direct to hompage */}
-        <Link to={"/"} href="" className="flex items-center gap-2">
+        <Link to={"/"} href="" className="flex items-center gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
             viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
+            fill="#93C5FD"
             className="size-10"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"
+              fillRule="evenodd"
+              d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+              clipRule="evenodd"
             />
           </svg>
-          <span className="font-bold text-xl">DreamStay</span>
+
+          <span className="font-bold text-xl text-blue-300">DreamStay</span>
         </Link>
         {/* search area */}
         <div className="flex justify-center border border-neutral-300 rounded-full py-2 px-8 shadow-md shadow-gray-300 gap-5">
