c09d2fb425806fcb827325d6c3d1da56ad9712b8
Xiahui Zhang <xhz0729@gmail.com>
2024-11-06 15:49:29 -0600

    Test search area render correctly

--- client/src/__tests__/Header.test.jsx
+++ client/src/__tests__/Header.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import Header from "../components/Header";
+import { expect } from "vitest";
 
 describe("Header component", () => {
   // Test that clicking on the company name navigates to the homepage (‘/‘)
@@ -21,5 +22,25 @@ describe("Header component", () => {
 
     // Assert that the link has the correct href attribute that points to ‘/‘
     expect(companyLink).toHaveAttribute("href", "/");
+  });
+
+  // Test that render the Header component with search area
+  it("renders the search area", () => {
+    render(
+      <UserContext.Provider value={{ user: null }}>
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>
+      </UserContext.Provider>
+    );
+    // Assert that the search area is rendered
+    const Where = screen.getByText(/Where/i);
+    expect(Where).toBeInTheDocument();
+
+    const When = screen.getByText(/When/i);
+    expect(When).toBeInTheDocument();
+
+    const Guests = screen.getByText(/Guests/i);
+    expect(Guests).toBeInTheDocument();
   });
 });
